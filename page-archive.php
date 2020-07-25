7af233bbde13295c94be721abe82a28080aee29b
JollyWizard <james.arlow@gmail.com>
2020-07-25 15:00:07 -0400

    Transitional archive format (old and new meta).
    The old wordpress metadata was generated with a cutom fields plugin that
    produced hard to use results (serial date formats) and inconsistent key names.
    
    The metadata was migrated with cli tools, but the live site had to support both
    metadata formats to prevent breakage during transition.
    
    I referred to this internally as the transition theme. I set this as v1.
    It is the last version of my original visual draft to run on the live site,
    and it is intended to hold the site over until a more comprehensive
    update is available.
    
    This visual scheme ran on the site for over a year.

--- page-archive.php
+++ page-archive.php
@@ -21,9 +21,28 @@ get_header();
 	  $args = array(
 		'post_type' => 'session'
 	  , 'nopaging' => 'true'
-	  , 'meta_key' => 'wpcf-number'
-	  , 'order' => 'ASC'
-	  , 'orderby' => 'meta_value_num'
+	
+	  /* Legacy theme uses wpcf namespace (from custom fields plugin tool). */
+	  //, 'meta_key' => 'wpcf-number'
+	  
+	  /* Transition theme accepts both key formats. */
+	  , 'meta_query' => array(
+			'relation' => 'OR'
+		,	'wpcf-format' => array (
+				'key' => 'wpcf-number'
+			,	'type' => 'NUMERIC'
+			)
+		,	'hf-format' => array (
+				'key' => 'hf-session-number'
+			,	'type' => 'NUMERIC'
+			)
+		)
+
+	  /* Old setting was 'meta_value_num', but transition uses complex query and can't use that. */
+	  /* Named meta queries is prefered syntax */
+	  , 'orderby' => array(
+		  'wpcf-format' => 'ASC', 'hf-format' => 'ASC'
+	  	)
 	  );
 	  $wp_query = new WP_Query($args);
 	  ?>
@@ -33,6 +52,10 @@ get_header();
 		<?php
 		while ( have_posts() ) : the_post();
 		$session_number = get_post_meta(get_the_ID(), 'wpcf-number', true);
+		if ( ! $session_number ) 
+		{
+			$session_number = get_post_meta(get_the_ID(), 'hf-session-number', true);
+		}
 		?>
 			<a href='<?php echo get_permalink() ?>'>
 				<?php echo $session_number ?>
